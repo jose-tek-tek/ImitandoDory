9c3e223d548db92370b20704112edffdf1d93a0d
Pedrox | Adm! Dev! <163602203+jose-tek-tek@users.noreply.github.com>
2024-03-31 17:21:31 -0300

    ImitandoDooory

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Tradutor Baleês");
+Console.WriteLine();
+
+Console.WriteLine("digite o texto a traduzir para o Baleês:");
+Console.WriteLine();
+
+String texto = Console.ReadLine()!;
+
+Console.WriteLine("\nem baleês seria...:\n");
+
+Console.WriteLine(texto
+.Replace("a" , "aaa")
+.Replace("e" , "eee")
+.Replace("i" , "iii")
+.Replace("o" , "ooo")
+.Replace("u" , "uuu")
+);
